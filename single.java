72424adfeaad2de978e4353c59b92e54c4b74169
sharkMahmoodi <108240269+masmahmoodi@users.noreply.github.com>
2023-11-02 23:07:11 +0430

    Add files via upload

---
+++ single.java
@@ -0,0 +1,222 @@
+
+package singlelinkedlist;
+
+
+public class SingleLinkedList {
+
+    
+    public static void main(String[] args) {
+        
+        
+        // run code here
+        
+        
+        
+        
+    }
+    
+}
+
+
+public class linkedListOperation {
+
+    private Node head;
+    private Node tail;
+    private int length;
+    // inner class for creating Nodes
+    class Node{
+        int value;
+        Node next;
+        
+        // constructor of Node class
+        Node(int value){
+            this.value = value;
+        }
+        
+        
+        
+    }
+    
+    // constructor of linkedListOperation
+
+    public linkedListOperation(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+        
+    }
+    
+    
+    
+	
+   //print element method 
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+
+
+    //get hed method 
+    public void getHead() {
+        if (head == null) {
+            System.out.println("Head: null");
+        } else {
+            System.out.println("Head: " + head.value);
+        }
+    }
+    
+
+
+    //get tail method 
+    public void getTail() {
+        if (head == null) {
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Tail: " + tail.value);
+        }
+    }
+
+
+
+
+    //get length method 
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+    
+    
+      //append method 
+     public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+
+   
+    //remove last  method 
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+
+
+    //prepend method 
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+
+    
+    //remove first method 
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+
+
+    //get method 
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for(int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+
+
+    //set method 
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+
+   //insert method 
+    public boolean insert(int index, int value)  {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    
+    //remove method 
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+  
+
+
+}
+
+
